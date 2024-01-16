3902d4f3e355c8a44b8d33fe63c2f1fadbe3b859
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2024-01-16 10:34:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
-//complete this code
 class Animal {
-	constructor(species){
-		this._species=species
-	}
+  constructor(species) {
+    this._species = species;
+  }
 
-	get species(){
-		return this._species
-	}
+  get species() {
+    return this._species;
+  }
 
-	makeSound(){
-		console.log("Generic animal sound")
-	}
-}
-
-class Dog extends Animal {
-	bark(){
-		console.log("woof")
-	}
+  makeSound() {
+    console.log("The " + this.species + " makes a sound");
+  }
 }
 
 class Cat extends Animal {
-	purr(){
-		console.log("purr")
-	}
+  purr() {
+    console.log("purr");
+  }
 }
 
-const genericAnimal = new Animal("Unknown");
-console.log(genericAnimal.species); // Output: Unknown
-genericAnimal.makeSound(); // Output: Generic animal sound
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
 
-const fluffy = new Cat("Cat");
-console.log(fluffy.species); // Output: Cat
-fluffy.makeSound(); // Output: Generic animal sound
-fluffy.purr(); // Output: purr
+const animal = new Animal("Unknown");
+console.log(animal.species); 
+animal.makeSound(); 
 
-const rover = new Dog("Dog");
-console.log(rover.species); // Output: Dog
-rover.makeSound(); // Output: Generic animal sound
-rover.bark(); // Output: woof
+const cat = new Cat("Cat");
+console.log(cat.species); 
+cat.makeSound(); 
+cat.purr();
+
+const dog = new Dog("Dog");
+console.log(dog.species); 
+dog.makeSound(); 
+dog.bark(); 
 
 // Do not change the code below this line
 window.Animal = Animal;
